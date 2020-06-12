8115d1c8a848dc5580577628a8b14f641bb08bfb
Felipe <felipemendes@me.com>
2020-06-12 17:45:49 -0300

    Add infrastructure layer

---
+++ InfrastructureTests/AlamofireAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  AlamofireAdapterTests.swift
+//  InfrastructureTests
+//
+//  Created by Felipe Ribeiro Mendes on 12/06/20.
+//  Copyright © 2020 Felipe Mendes. All rights reserved.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+    func test_() {
+
+    }
+}
