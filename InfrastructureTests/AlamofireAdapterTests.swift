8af53483510c9412dc67c2e2d6a87b6dbe19e7f4
Felipe <felipemendes@me.com>
2020-06-13 17:37:14 -0300

    Ensure AlamofireAdapter can call Alamofire without data

--- InfrastructureTests/AlamofireAdapterTests.swift
+++ InfrastructureTests/AlamofireAdapterTests.swift
@@ -23,7 +23,7 @@ class AlamofireAdapter {
     // MARK: - PUBLIC API
 
     func post(to url: URL, with data: Data?) {
-        let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+        let json = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
         session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
@@ -43,6 +43,23 @@ class AlamofireAdapterTests: XCTestCase {
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("POST", request.httpMethod)
             XCTAssertNotNil(request.httpBodyStream)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+
+    func test_post_should_make_request_without_data() {
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdapter(session: session)
+
+        sut.post(to: url, with: nil)
+
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.requestObserver { request in
+            XCTAssertNil(request.httpBodyStream)
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
