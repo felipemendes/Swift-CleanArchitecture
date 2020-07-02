0009b8966930fa8aec04f398bc2013f2c6fc4175
Felipe <felipemendes@me.com>
2020-07-01 22:52:12 -0300

    Ensure SignUpViewController has a loading and it's hidden by default

---
+++ UI/SignUpViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpViewController.swift
+//  UI
+//
+//  Created by Felipe Ribeiro Mendes on 01/07/20.
+//  Copyright © 2020 Felipe Mendes. All rights reserved.
+//
+
+import UIKit
+
+final class SignUpViewController: UIViewController {
+
+    // MARK: - UI
+
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+
+    // MARK: - LIFE CYCLE
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
