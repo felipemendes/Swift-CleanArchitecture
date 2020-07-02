a0e2b61aeff9f1a3d2ed778b9d8d3a6c8261a88c
Felipe <felipemendes@me.com>
2020-07-01 22:08:55 -0300

    Add UI layer

---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpViewControllerTests.swift
+//  UITests
+//
+//  Created by Felipe Ribeiro Mendes on 01/07/20.
+//  Copyright © 2020 Felipe Mendes. All rights reserved.
+//
+
+import XCTest
+
+class SignUpViewControllerTests: XCTestCase {
+    func test_() {
+
+    }
+}
