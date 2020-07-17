563c1a7f15cb2af526cabd72f94b6ee11d45c2f4
Felipe <felipemendes@me.com>
2020-07-16 21:29:39 -0300

    Add Validation layer

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  EmailValidatorAdapterTests.swift
+//  ValidationTests
+//
+//  Created by Felipe Ribeiro Mendes on 16/07/20.
+//  Copyright © 2020 Felipe Mendes. All rights reserved.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+    func test_() {
+
+    }
+}
