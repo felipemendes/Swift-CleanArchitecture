083734ce6ff5cb5d69ed9c50c745b56270914c6a
Felipe <felipemendes@me.com>
2020-07-29 15:32:10 -0300

    Ensure WelcomeRouter calls nav with loginFactory on goToLogin

---
+++ UITests/Scenes/Welcome/WelcomeRouterTests.swift
@@ -0,0 +1,48 @@
+//
+//  WelcomeRouterTests.swift
+//  UITests
+//
+//  Created by Felipe Ribeiro Mendes on 28/07/20.
+//  Copyright © 2020 Felipe Mendes. All rights reserved.
+//
+
+import UI
+import UIKit
+import XCTest
+
+public final class WelcomeRouter {
+
+    // MARK: - PRIVATE PROPERTIES
+
+    private let navigation: NavigationController
+    private let loginFactory: () -> LoginViewController
+
+    // MARK: - PUBLIC API
+
+    public init(navigation: NavigationController,
+                loginFactory: @escaping () -> LoginViewController) {
+        self.navigation = navigation
+        self.loginFactory = loginFactory
+    }
+
+    func goToLogin() {
+        navigation.pushViewController(loginFactory())
+    }
+}
+
+class WelcomeRouterTests: XCTestCase {
+    func test_goToLogin_calls_nav_with_correct_viewController() {
+        let navigation = NavigationController()
+        let loginFactorySpy = LoginFactorySpy()
+        let sut = WelcomeRouter(navigation: navigation, loginFactory: loginFactorySpy.makeLogin)
+        sut.goToLogin()
+        XCTAssertEqual(navigation.viewControllers.count, 1)
+        XCTAssertTrue(navigation.viewControllers[0] is LoginViewController)
+    }
+
+    class LoginFactorySpy {
+        func makeLogin() -> LoginViewController {
+            return LoginViewController.instantiate()
+        }
+    }
+}
